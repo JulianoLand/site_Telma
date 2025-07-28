c5d7e7c9e4bb2cdf882bbe86d5173698197db664
JulianoLand <juliannojp@gmail.com>
2025-07-28 01:15:03 -0300

    animação icon principal

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,10 @@
  /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./src/**/*.{html,js}"],
+  content: ["./**/*.{html,js}"],
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [
+    require('tailwindcss-animated')
+  ],
 }
